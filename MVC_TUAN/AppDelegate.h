7f4fb2ba347bb137802fc2e38e7245419d24bed7
李翔 <lixiangflyin@163.com>
2014-07-02 16:39:33 +0800

    Initial Commit

---
+++ MVC_TUAN/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MVC_TUAN
+//
+//  Created by apple on 14-7-2.
+//  Copyright (c) 2014年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MVC_TUANTests/MVC_TUANTests.m
@@ -0,0 +1,34 @@
+//
+//  MVC_TUANTests.m
+//  MVC_TUANTests
+//
+//  Created by apple on 14-7-2.
+//  Copyright (c) 2014年 apple. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MVC_TUANTests : XCTestCase
+
+@end
+
+@implementation MVC_TUANTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
