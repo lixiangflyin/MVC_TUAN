b60832cc205c147a3822475c0f631334d86865b6
李翔 <lixiangflyin@163.com>
2014-08-26 09:23:17 +0800

    改变
    改变

--- MVC_TUAN/LocationManager.h
+++ MVC_TUAN/LocationManager.h
@@ -1,9 +1,9 @@
 //
 //  LocationManager.h
-//  TongTongTuan
+//  MVCdemo
 //
-//  李红(410139419@qq.com)创建于 13-7-23.
-//  Copyright (c) 2013年 贵阳世纪恒通科技有限公司. All rights reserved.
+//  Created by apple on 14-7-2.
+//  Copyright (c) 2014年 apple. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
